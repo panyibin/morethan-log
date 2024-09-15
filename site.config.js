9f937c144b2c86cbe8fc50bd4fbbb60729fd072f
yibin <yibinpan@sending.tech>
2024-09-15 18:14:57 +0800

    update:title

--- site.config.js
+++ site.config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `ximenchuixie-blog`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
